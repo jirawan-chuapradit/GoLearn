6ccdd912762b85236d696aae9efbabd385f0b3ca
Jirawan.C <jirawan.c@teohong.com>
2020-07-09 10:39:40 +0700

    struct encode and decode

--- Project-Create-a-Log-Parser/main.go
+++ Project-Create-a-Log-Parser/main.go
@@ -4,30 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
-//func main (){
-//
-//	os.Stdin.Close()
-//	in := bufio.NewScanner(os.Stdin)
-//	//in.Scan() // buffer input
-//
-//	var lines int
-//
-//	for in.Scan() {
-//		lines ++
-//
-//	}
-//	fmt.Printf("There are %d line(s)\n", lines)
-//
-//	if err := in.Err(); err != nil {
-//		fmt.Println("Error:", err)
-//	}
-//
-//	//fmt.Println("Scanned Bytes:", in.Bytes())
-//}
+func main (){
+
+	//os.Stdin.Close()
+	in := bufio.NewScanner(os.Stdin)
+	//in.Scan() // buffer input
+
+	var lines int
+
+	for in.Scan() {
+		lines ++
+
+	}
+	fmt.Printf("There are %d line(s)\n", lines)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	//fmt.Println("Scanned Bytes:", in.Bytes())
+}
 
 //func main(){
 //	rx := regexp.MustCompile(`[^a-z]+`)
@@ -61,54 +59,54 @@ import (
 //	fmt.Println("sorry the input does not contain the word.")
 //}
 
-func main() {
-	var (
-		sum map[string]int
-		domains []string
-		total int
-		lines int
-		)
-	sum = make(map[string]int)
-
-	in := bufio.NewScanner(os.Stdin)
-
-	for in.Scan() {
-		lines++
-		fields := strings.Fields(in.Text())
-		//fmt.Printf("domain: %s - visits: %s\n", fields[0], fields[1])
-
-		if len(fields) != 2 {
-			fmt.Printf("wrong input: %v (line #%d)\n", fields, lines)
-			return
-		}
-
-		domain := fields[0]
-
-		visits, err := strconv.Atoi(fields[1])
-		if visits <0|| err != nil {
-			fmt.Printf("wrong input: %q(line #%d)\n", fields[1], lines)
-			return
-		}
-
-
-		if _, ok := sum[domain]; ok {
-			domains = append(domains, domain)
-		}
-		total += visits
-		sum[domain] += visits
-	}
-	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
-	fmt.Println(strings.Repeat("-", 45))
-
-
-	for _, domain := range domains {
-		visits := sum[domain]
-		fmt.Printf("%-30s %10d\n", domain, visits)
-	}
-
-	fmt.Printf("\n%-30s %10d\n", "TOTAL", total)
-
-	if err := in.Err(); err != nil {
-		fmt.Println("> Err:", err)
-	}
-}
+//func main() {
+//	var (
+//		sum map[string]int
+//		domains []string
+//		total int
+//		lines int
+//		)
+//	sum = make(map[string]int)
+//
+//	in := bufio.NewScanner(os.Stdin)
+//
+//	for in.Scan() {
+//		lines++
+//		fields := strings.Fields(in.Text())
+//		//fmt.Printf("domain: %s - visits: %s\n", fields[0], fields[1])
+//
+//		if len(fields) != 2 {
+//			fmt.Printf("wrong input: %v (line #%d)\n", fields, lines)
+//			return
+//		}
+//
+//		domain := fields[0]
+//
+//		visits, err := strconv.Atoi(fields[1])
+//		if visits <0|| err != nil {
+//			fmt.Printf("wrong input: %q(line #%d)\n", fields[1], lines)
+//			return
+//		}
+//
+//
+//		if _, ok := sum[domain]; ok {
+//			domains = append(domains, domain)
+//		}
+//		total += visits
+//		sum[domain] += visits
+//	}
+//	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
+//	fmt.Println(strings.Repeat("-", 45))
+//
+//
+//	for _, domain := range domains {
+//		visits := sum[domain]
+//		fmt.Printf("%-30s %10d\n", domain, visits)
+//	}
+//
+//	fmt.Printf("\n%-30s %10d\n", "TOTAL", total)
+//
+//	if err := in.Err(); err != nil {
+//		fmt.Println("> Err:", err)
+//	}
+//}
---
+++ Structs-Encode-and-Decode-Json/main.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+//func main() {
+//	var (
+//		name, lastname string
+//		age            int
+//
+//		name2, lastname2 string
+//		age2             int
+//	)
+//
+//	name, lastname, age = "pablo", "picasso", 91
+//	name2, lastname2, age2 = "sigmund", "freud", 83
+//
+//	fmt.Println("Picasso: ", name, lastname, age)
+//	fmt.Println("Freud: ", name2, lastname2, age2)
+//
+//
+//	type person struct {
+//		name, lastname string
+//		age            int
+//	}
+//
+//	picasso := person{name:"pablo", lastname:"Picasso", age:91}
+//
+//	var freud person
+//
+//
+//
+//	freud.name = "sigmund"
+//	freud.lastname = "freud"
+//	freud.age = 83
+//
+//	fmt.Printf("\nPicasso: %#v\n",picasso)
+//	fmt.Printf("\nFreud: %+v\n",freud)
+//}
+
+type song struct {
+	title, artist string
+}
+
+type playlist struct {
+	genre string
+
+	songs []song
+}
+
+func main() {
+	//song1 := song{title:"wonderwall", artist:"oasis"}
+	//song2 := song{title: "super sonic", artist:"oasis"}
+	//
+	//fmt.Printf("song1: %+v\nsong2: %+v\n", song1,song2)
+
+	//song1 = song2
+	//if song1==song2{
+	//	fmt.Println("songs are equal.")
+	//}else{
+	//	fmt.Println("songs are not equal.")
+	//}
+
+	//rock := playlist{
+	//	genre: "indie rock", songs: []song{
+	//		song{title: "wonderwall", artist: "oasis"},
+	//		song{title: "super sonic", artist: "oasis"},
+	//	},
+	//}
+
+	songs := []song{
+		song{title: "wonderwall", artist: "oasis"},
+		song{title: "super sonic", artist: "oasis"},
+	}
+
+	rock := playlist{
+		genre: "indie rock",
+		songs: songs,
+	}
+
+	fmt.Printf("\n%-20s %20s\n", "Title", "artist")
+
+	song := rock.songs[0]
+	song.title = "live forever"
+
+	for _, s := range rock.songs {
+		fmt.Printf("%-20s %20s\n", s.title, s.artist)
+	}
+
+}
